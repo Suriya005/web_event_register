d0cdb03d194c95ae2f12cbfc7651610f64799ea6
Suriya005 <45837558+Suriya005@users.noreply.github.com>
2021-10-12 09:33:16 +0700

    u

--- font-end/src/environments/environment.prod.ts
+++ font-end/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  serverUrl: 'http://localhost:3000',
+  serverUrl: 'http://127.0.0.1:3000',
 };
--- font-end/src/environments/environment.ts
+++ font-end/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  serverUrl: 'http://localhost:3000',
+  serverUrl: 'http://127.0.0.1:3000',
 };
 
 /*
