be41b4fffb29f7f7fe34e0929630add32184253c
Suriya005 <45837558+Suriya005@users.noreply.github.com>
2021-10-13 13:51:00 +0700

    u

--- font-end/src/app/admin/forms/add-major/add-major.component.ts
+++ font-end/src/app/admin/forms/add-major/add-major.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-major',
@@ -9,7 +10,20 @@ export class AddMajorComponent implements OnInit {
 
   constructor() { }
 
+  addMajor = new FormGroup({
+    majorName: new FormControl(''),
+    latitude: new FormControl(''),
+    longitude: new FormControl(''),
+  });
+
   ngOnInit(): void {
+
+  }
+  add() {
+    console.log(this.addMajor.value);
+    // this.loginService.registerUser(this.registerForm.value).then((res:any)=>{
+    //   console.log(res);
+    // })
   }
 
 }
